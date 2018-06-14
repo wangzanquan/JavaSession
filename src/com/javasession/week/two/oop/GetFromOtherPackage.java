432a8b84505b000b1fd91f2e3a7ed73869ee7ad3
wangzanquan <wangzanquan@yahoo.com>
2018-06-14 10:16:37 -0400

    06142018

---
+++ src/com/javasession/week/two/oop/GetFromOtherPackage.java
@@ -0,0 +1,12 @@
+package com.javasession.week.two.oop;
+
+//import package
+import com.javasession.week.two.tryandcatch.TryCatchSample;
+
+public class GetFromOtherPackage {
+	
+	public static void main(String[] args) {
+		TryCatchSample tc = new TryCatchSample();
+	}
+
+}
